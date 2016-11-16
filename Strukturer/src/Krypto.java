93dd7416923842b11a857797cf0136385c63776d
ZozkMohamed <zozk.mohamed@elev.it-gymnasiet.se>
2016-11-16 10:34:59 +0100

    2016-11-16

---
+++ Strukturer/src/Krypto.java
@@ -0,0 +1,19 @@
+
+public class Krypto {
+	static int krypto - new int {
+		{1,2,3,3,3},
+		{2,3,4,15,1},
+		{13,4,5,1,2},
+		{5,1,2,3,4};
+		
+	static char key = new char ;
+	static int sizeX-5, sizeY=5; //Storlek på Krypto
+	
+	Scanner keyb = new Scanner(System.in);
+	
+	KryptoSolver(){
+		;
+		
+	}
+	}
+}
